f891165590cdea3ec6a94fc30d6c904445f6508b
Morteza Mirzaei <dev.mirzaei@gmail.com>
2023-05-29 13:34:27 +0430

    add filter scope and tags relation to `News` model

--- app/Models/News.php
+++ app/Models/News.php
@@ -16,4 +16,46 @@ class News extends Model
         'source',
         'registered_at',
     ];
+
+    /**
+     * Get the news's registered_at
+     *
+     * @param  datetime  $value
+     * @return integer
+     */
+    public function getRegisteredAtAttribute($value)
+    {
+        return strtotime($value);
+    }
+
+    /**
+     * Set the news's registered_at
+     *
+     * @param  integer  $value
+     * @return void
+     */
+    public function setRegisteredAtAttribute($value)
+    {
+        $this->attributes['registered_at'] = date('Y-m-d H:i:s', (int) $value / 1000);
+    }
+
+    /**
+     * Search filter query
+     */
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            $query->where('title', 'LIKE', '%' . $search . '%')
+                ->orWhere('url', 'LIKE', '%' . $search . '%')
+                ->orWhere('reference', 'LIKE', '%' . $search . '%');
+        });
+    }
+
+    /**
+     * Get all tags for the specified resource
+     */
+    public function tags(): \Illuminate\Database\Eloquent\Relations\MorphToMany
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
 }
